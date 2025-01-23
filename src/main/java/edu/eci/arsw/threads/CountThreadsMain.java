5718f776afa4c7b04d1c96ef334ec8f59592de16
Lattes_and_Cream <santiago.guerra@mail.escuelaing.edu.co>
2025-01-23 09:55:03 -0500

    Primer Commit. Parte I desarrollada

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -9,10 +9,18 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
+
+import edu.eci.arsw.threads.CountThread;
 public class CountThreadsMain {
     
     public static void main(String a[]){
-        
+        CountThread hilo1 = new CountThread(0,99);
+        CountThread hilo2 = new CountThread(99,199);
+        CountThread hilo3 = new CountThread(200,299);
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+
     }
     
 }
