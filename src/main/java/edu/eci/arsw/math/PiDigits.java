0878c24ca95229780f7d7fccd98b9466844f6ea8
andres Rodriguez <rodriguezandres160918@gmail.com>
2025-01-25 20:04:44 -0500

    Update

--- src/main/java/edu/eci/arsw/math/PiDigits.java
+++ src/main/java/edu/eci/arsw/math/PiDigits.java
@@ -16,40 +16,28 @@ public class PiDigits {
     private static List<PidigitsThread> ListThread = new ArrayList<PidigitsThread>();
     private static byte[] ListByte;
 
-    /**
-     * Returns a range of hexadecimal digits of pi.
-     * 
-     * @param start The starting location of the range.
-     * @param count The number of digits to return
-     * @return An array containing the hexadecimal digits.
-     */
     public byte[] getDigits(int start, int count, int n) {
 
-        // Reiniciar la lista de hilos eliminando cualquier hilo que este previamente guardado
-
+        ListThread.clear();
 
         // Se calcula la cantidad de digitos que cada hilo debe procesar
         int ThreadNumber = count / n;
         ListByte = new byte[count];
         int auxiliar = 0;
-
-        //Crear el primer hilo con la mayor carga de trabajo adoptando el excendente
+        //Crear el primer hilo con la mayor carga de trabajo adoptando el excedente
         if (count % n != 0)
             auxiliar = count % n;
         ListThread.add(new PidigitsThread(start, ThreadNumber + auxiliar));
         n--;
         start += auxiliar;
-        
         // Se crean los demas hilos
         for (int i = 0; i < n; i++) {
             ListThread.add(new PidigitsThread(start + ThreadNumber, ThreadNumber));
             start += ThreadNumber;
         }
-
          // Aqui arrancan todos los hilos
         for (Thread T : ListThread) {
             T.start();
-
         }
         // Sincronizar los hilos y se utiliza la funcion join para que el hilo
         // principal espere que cada hilo termine su ejecucion
